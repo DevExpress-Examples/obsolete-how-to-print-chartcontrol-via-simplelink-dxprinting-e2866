9d7d2d2a1d0d9fb33213fe7e37b5d4bdd88c7932
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using DevExpress.Xpf.Printing;
+
+namespace WpfApplication1 {
+
+    public partial class Window1 : Window {
+        public Window1 () {
+            InitializeComponent();
+        }
+
+        private void button1_Click (object sender, RoutedEventArgs e) {
+            SimpleLink sl = new SimpleLink();
+            sl.DetailCount = 1;
+            sl.DetailTemplate = (DataTemplate)Resources["Data"];
+            sl.CreateDetail += new EventHandler<CreateAreaEventArgs>(sl_CreateDetail);
+            sl.CreateDocument(true);
+            sl.ShowPrintPreviewDialog(this);
+        }
+
+        void sl_CreateDetail (object sender, CreateAreaEventArgs e) {
+            VisualBrush brush = new VisualBrush(chartControl1);
+            DrawingVisual visual = new DrawingVisual();
+            DrawingContext context = visual.RenderOpen();
+
+            context.DrawRectangle(brush, null,
+                new Rect(0, 0, chartControl1.ActualWidth, chartControl1.ActualHeight));
+            context.Close();
+
+            RenderTargetBitmap bmp = new RenderTargetBitmap((int)chartControl1.ActualWidth,
+                (int)chartControl1.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+
+            bmp.Render(visual);
+            e.Data = bmp;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# OBSOLETE - How to print ChartControl via SimpleLink (DXPrinting)
+
+
+This example illustrates an approach of including a <strong>ChartControl</strong> picture into a document generated via <strong>SimpleLink</strong> (DXPrinting).
+<p>A corresponding functionality should be available out-of-the-box in a future update (see <a href="http://www.devexpress.com/issue=S32927">http://www.devexpress.com/issue=S32927</a>).<br /> In the example, a chart's image is obtained via the <strong>RenderTargetBitmap.Render</strong> method.<br /><br /><strong>Note!</strong> Starting from v2014 vol 1, the chart control contains the built-in <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfChartsChartControl_Printtopic">ChartControl.Print</a>  and <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfChartsChartControl_ShowPrintPreviewtopic">ChartControl.ShowPrintPreview</a>  methods. To learn how to work with methods, refer to the <a href="https://www.devexpress.com/Support/Center/p/T139338">How to print a chart and show Its print preview</a>   example.</p>
+
+<br/>
+
+
